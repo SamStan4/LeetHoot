2c52224cf6eb3101254c094a8c89472e6e1a188b
samStan4 <sam.charles.stanley@gmail.com>
2025-02-15 14:25:41 -0800

    config done?

---
+++ server/src/index.ts
@@ -0,0 +1,14 @@
+import TestClass from "@common/types/thing"
+
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, TypeScript Express!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});

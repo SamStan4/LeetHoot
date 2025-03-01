39e010d4558a1ab6991591b33e87524550e46999
samStan4 <sam.charles.stanley@gmail.com>
2025-03-01 09:33:47 -0800

    checkpoint

--- client/src/pages/JoinGamePage.jsx
+++ client/src/pages/JoinGamePage.jsx
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom';
 export default function JoinGamePage() {
   const { "game-id": gameId } = useParams();
   return (
-    <div>
+    <div className="h-screen flex flex-col justify-center items-center bg-blue-100">
       <h1>Join a Game : {gameId}</h1>
     </div>
   );

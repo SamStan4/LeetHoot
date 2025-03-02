52efae1372eb34bd083daeccd4d219b1ae65f19c
KorbinWeiler <korbinweiler@gmail.com>
2025-03-02 10:56:27 -0800

    Add join-game page content

--- client/src/pages/JoinGamePage.jsx
+++ client/src/pages/JoinGamePage.jsx
@@ -1,10 +1,21 @@
 import { useParams } from 'react-router-dom';
 
+//Probably going to end up using this to get both the game pin and name 
+//since those pages are almost identical
 export default function JoinGamePage() {
   const { "game-id": gameId } = useParams();
   return (
     <div>
-      <h1>Join a Game : {gameId}</h1>
-    </div>
+            <input className="host" type="button" value="host"></input>
+            <h1>LeetHoot</h1>
+            <div className="box">
+                <div>
+                    <input className="game-pin" type="text" placeholder="Game PIN"></input>
+                </div>
+                <div>
+                    <button className="enter">Enter</button>
+                </div>
+            </div>
+        </div>
   );
 }

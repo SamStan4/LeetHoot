47c52618ff846a8f82398e0190852936a58294e5
KorbinWeiler <korbinweiler@gmail.com>
2025-03-02 11:37:21 -0800

    added some initial styling to the join-game page

--- client/src/pages/JoinGamePage.jsx
+++ client/src/pages/JoinGamePage.jsx
@@ -1,4 +1,5 @@
 import { useParams } from 'react-router-dom';
+import "./JoinGamePage.css"
 
 //Probably going to end up using this to get both the game pin and name 
 //since those pages are almost identical
@@ -6,16 +7,16 @@ export default function JoinGamePage() {
   const { "game-id": gameId } = useParams();
   return (
     <div>
-            <input className="host" type="button" value="host"></input>
-            <h1>LeetHoot</h1>
-            <div className="box">
-                <div>
-                    <input className="game-pin" type="text" placeholder="Game PIN"></input>
-                </div>
-                <div>
-                    <button className="enter">Enter</button>
-                </div>
+        <input className="bg-gray-700 hover:bg-blue-700 border text-gray-100 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline host-button" type="button" value="host"></input>
+        <h1 className='text-white'>LeetHoot</h1>
+        <div className="bg-gray-700 rounded border border-gray-100 game-values">
+            <div>
+                <input className="bg-gray-700 shadow appearance-none border rounded w-full py-2 px-3 text-gray-100 leading-tight focus:outline-none focus:shadow-outline user-input" type="text" placeholder="Game PIN"></input>
+            </div>
+            <div>
+                <input className='bg-gray-700 hover:bg-blue-700 text-gray-100 font-bold py-2 px-4 rounded border border-gray-100 focus:outline-none focus:shadow-outline enter-button' type='button' value='Enter'></input>
             </div>
         </div>
+    </div>
   );
 }

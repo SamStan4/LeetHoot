f8eacfd821c881ee2316cd0fccd3d8957b0373ac
KorbinWeiler <korbinweiler@gmail.com>
2025-03-02 12:14:03 -0800

    created play-game page

--- client/src/pages/PlayGamePage.jsx
+++ client/src/pages/PlayGamePage.jsx
@@ -3,8 +3,10 @@ import { useParams } from 'react-router-dom';
 export default function PlayGamePage() {
   const { "game-id": gameId } = useParams();
   return (
-    <div>
-      <h1>Play a Game : {gameId}</h1>
+    <div className='rounded bg-gray-800'>
+      <p className='text-white m-[5px]'>Code:</p>
+      <input type='text' className='rounded bg-gray-600 w-[99%] h-[70vh] m-[5px]'></input>
+      <input type='button' className='rounded border bg-green-500 justify-items-center m-[5px] w-[5%]' value='Submit'></input>
     </div>
   );
 }

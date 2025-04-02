8a18a126c20930d390d360eff159464fdf11120b
samStan4 <sam.charles.stanley@gmail.com>
2025-04-02 08:54:51 -0700

    added some new sql

--- server/sql/dbInit.sql
+++ server/sql/dbInit.sql
@@ -1,5 +1,12 @@
 CREATE TABLE IF NOT EXISTS QuestionTable (
-    questionName TEXT NOT NULL,
-    questionDifficulty TEXT NOT NULL,
-    PRIMARY KEY (questionName)
+  questionName TEXT NOT NULL,
+  questionDifficulty TEXT NOT NULL,
+  PRIMARY KEY (questionName)
+);
+
+-- There is going to be some triggers on this table that will trip some web sockets
+CREATE TABLE IF NOT EXISTS GameTable (
+  gameID INTEGER PRIMARY KEY AUTOINCREMENT,
+  gameQuestions TEXT NOT NULL,
+  gameState INTEGER DEFAULT -1
 );
---
+++ server/sql/inserts.sql
@@ -0,0 +1,4 @@
+INSERT INTO GameTable (GameQuestions) 
+VALUES
+  ('{ "questions": ["two-sum", "invert-binary-tree"] }'),
+  ('{ "questions": ["two-sum"] }');

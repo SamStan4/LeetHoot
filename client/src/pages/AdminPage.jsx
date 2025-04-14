29504a562ac9bfc4f9bfdcae74f732b6978a08e1
KorbinWeiler <korbinweiler@gmail.com>
2025-04-13 22:26:52 -0700

    improved the styling for the scroll bar

--- client/src/pages/AdminPage.jsx
+++ client/src/pages/AdminPage.jsx
@@ -51,7 +51,11 @@ export default function AdminPage(){
                 <div className="w-[80%] h-[80%] align-text-top">
                     <h1 className="justify-left align-text-top">Sessions</h1>
                     <hr className="w-[100%]"/>
-                    <div className="h-100 overflow-y-auto">
+                    <div className="h-100 overflow-y-auto [&::-webkit-scrollbar]:w-2
+  [&::-webkit-scrollbar-track]:bg-gray-100
+  [&::-webkit-scrollbar-thumb]:bg-gray-300
+  dark:[&::-webkit-scrollbar-track]:bg-neutral-700
+  dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500">
                         {sessionList}
                     </div>
                 </div>
